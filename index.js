484ff095b934eb0e1eb6ee2d8c1bb44082deb472
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-11-26 16:01:09 +0600

    added user role found oparetion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,7 @@ async function run() {
 
     const apartmentCollection = client.db('assignment-twelve').collection('apartment');
     const usersCollection = client.db('assignment-twelve').collection('users');
+    const agreementAcceptCollection = client.db('assignment-twelve').collection('agreementAccept');
     const cartApartmentCollection = client.db('assignment-twelve').collection('cartApartment');
 
   // user collection here;
@@ -60,14 +61,55 @@ async function run() {
 
    app.post('/apartment', async(req, res) => {
        const apartmentData = req.body;
-       const query = {id: apartmentData.id};
-       const findData = await cartApartmentCollection.findOne(query);
-       if(findData){
-            return res.send({message: 'already added'})
-       }
        const result = await cartApartmentCollection.insertOne(apartmentData);
        res.send(result);
 
+   })
+
+   app.get('/cart-apartment', async(req, res) => {
+
+         const result = await cartApartmentCollection.find().toArray();
+         res.send(result)
+
+   })
+
+   app.patch('/update-reject/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const updateDoc = {
+          $set:{ status: 'checked'}
+        }
+        const result = await cartApartmentCollection.updateOne(query,updateDoc);
+        res.send(result); 
+   })
+
+   app.post('/update-confirm', async(req, res) => {
+         const {roomData} = req.body;
+         const statusQuery = {_id: new ObjectId(roomData._id)};
+         const updateDocStatus = {
+             $set: { status: 'checked'}
+         }
+
+        const resultStatus = await cartApartmentCollection.updateOne(statusQuery,updateDocStatus);
+
+        const userQuery = {email: roomData.useEmail};
+        const updateRole = {
+            $set: { role: 'member'}
+        };
+
+        const resultRole = await usersCollection.updateOne(userQuery, updateRole);
+
+        delete roomData._id;
+        const result = await agreementAcceptCollection.insertOne(roomData);
+      
+        res.send({resultStatus, resultRole, result})
+   })
+
+   app.get('/user-role/:email', async(req, res) => {
+         const email = req.params.email;
+         const query = {email: email};
+         const result = await usersCollection.findOne(query);
+         res.send(result); 
    })
 
 
